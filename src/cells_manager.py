297b2e55ca968b53b32f3df725466518ed16858f
Hanchun Wang <hw1121@ic.ac.uk>
2025-01-29 09:55:15 +0000

    Add the dividable cell to ensure simulation have two cell at the beginning

--- src/cells_manager.py
+++ src/cells_manager.py
@@ -57,6 +57,28 @@ def init_normal_cell(cell_id: int, born_time: float, life_time_func, division_ti
         will_divide=division_time < life_time
     )
 
+def init_dividable_cell(cell_id: int, born_time: float, life_time_func, division_time_func):
+    """
+    Initializes a cell with its properties, allowing division only if lifetime is greater than division time.
+
+    Parameters:
+    cell_id (int): Unique identifier for the cell.
+    born_time (float): Time when the cell is created.
+    life_time_func (function): Function to calculate the lifetime of the cell.
+    division_time_func (function): Function to calculate the division time of the cell.
+
+    Returns:
+    Cell object: A dataclass object representing the cell.
+    """
+    # try 100 times until the lifetime is greater than the division time
+    for _ in range(100):
+        cell = init_normal_cell(cell_id, born_time, life_time_func, division_time_func)
+        if cell.life_time > cell.division_time:
+            return cell
+    raise RuntimeWarning("Failed to create cell with lifetime > division time. check the sampling functions.")
+    return None
+
+
 def deactivate_cell(cell: Cell) -> None:
     """
     Disactivates a cell by marking it as inactive.
